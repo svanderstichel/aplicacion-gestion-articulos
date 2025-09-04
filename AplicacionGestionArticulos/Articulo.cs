9416c26b65819e70a03790da104cc2b24b386010
svanderstichel <vanderstichel@gmail.com>
2025-09-04 17:13:16 -0300

    Se creó el proyecto de WinForms y la clase Articulo con las propiedades mínimas requeridas.

---
+++ AplicacionGestionArticulos/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionGestionArticulos
+{
+    internal class Articulo
+    {
+        public int CodigoArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public string Categoria { get; set; }
+        public Image Imagen { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
