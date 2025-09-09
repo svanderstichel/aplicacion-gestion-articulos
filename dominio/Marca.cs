125d2593a24f411aa2177c0f9d35b2a06fffb735
svanderstichel <vanderstichel@gmail.com>
2025-09-09 11:40:39 -0300

    Se añadieron los direcotrios dominio y negocio

---
+++ dominio/Marca.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Marca
+    {
+        public int IdMarca {  get; set; }
+        public string Nombre { get; set; }
+
+        // agregue el usuario para que se pueda saber quien cargo el dato
+        public string UsuarioCarga { get; }    
+
+    }
+}
