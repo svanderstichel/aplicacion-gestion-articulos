c4792a8451475a649bf7eb9c5a78a11100b32747
Carolina gandin <carolina.gandin@axonier.com.ar>
2025-09-10 16:25:53 -0300

    subida de los demas archivos

---
+++ negocio/AltaArticuloNegocio.cs
@@ -0,0 +1,17 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class AltaArticuloNegocio
+    {
+        public void agregar(Articulo nuevo) { }
+        public void modificar (Articulo modificar) { }
+
+    }
+    
+}
