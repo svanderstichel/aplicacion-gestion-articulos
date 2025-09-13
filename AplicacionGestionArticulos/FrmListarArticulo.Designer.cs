7202c3dcb26ac2164d7207df58eed4c0df44bd20
svanderstichel <vanderstichel@gmail.com>
2025-09-13 13:34:43 -0300

    Se añadieron los filtros de articulos por diferentes criterios

--- AplicacionGestionArticulos/FrmListarArticulo.Designer.cs
+++ AplicacionGestionArticulos/FrmListarArticulo.Designer.cs
@@ -35,6 +35,11 @@
             this.lblListadoArticulos = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.btnAgregarArticuloDesdeLista = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFiltro = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvListadoArticulos)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,21 +57,25 @@
             // 
             // cmbCategoria
             // 
+            this.cmbCategoria.Cursor = System.Windows.Forms.Cursors.Default;
+            this.cmbCategoria.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbCategoria.FormattingEnabled = true;
             this.cmbCategoria.Location = new System.Drawing.Point(12, 50);
             this.cmbCategoria.Name = "cmbCategoria";
             this.cmbCategoria.Size = new System.Drawing.Size(121, 21);
             this.cmbCategoria.TabIndex = 0;
-            this.cmbCategoria.Text = "Categoría";
+            this.cmbCategoria.Tag = "";
+            this.cmbCategoria.SelectedIndexChanged += new System.EventHandler(this.cmbCategoria_SelectedIndexChanged);
             // 
             // cmbMarca
             // 
+            this.cmbMarca.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbMarca.FormattingEnabled = true;
             this.cmbMarca.Location = new System.Drawing.Point(139, 50);
             this.cmbMarca.Name = "cmbMarca";
             this.cmbMarca.Size = new System.Drawing.Size(121, 21);
             this.cmbMarca.TabIndex = 1;
-            this.cmbMarca.Text = "Marca";
+            this.cmbMarca.SelectedIndexChanged += new System.EventHandler(this.cmbMarca_SelectedIndexChanged);
             // 
             // btnEliminar
             // 
@@ -81,11 +90,12 @@
             // 
             this.lblListadoArticulos.AutoSize = true;
             this.lblListadoArticulos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblListadoArticulos.Location = new System.Drawing.Point(12, 22);
+            this.lblListadoArticulos.Location = new System.Drawing.Point(12, 9);
             this.lblListadoArticulos.Name = "lblListadoArticulos";
             this.lblListadoArticulos.Size = new System.Drawing.Size(120, 13);
             this.lblListadoArticulos.TabIndex = 4;
             this.lblListadoArticulos.Text = "Listado de artículos";
+            this.lblListadoArticulos.Click += new System.EventHandler(this.lblListadoArticulos_Click);
             // 
             // button1
             // 
@@ -100,13 +110,58 @@
             // 
             this.btnAgregarArticuloDesdeLista.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.btnAgregarArticuloDesdeLista.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnAgregarArticuloDesdeLista.Location = new System.Drawing.Point(685, 50);
+            this.btnAgregarArticuloDesdeLista.Location = new System.Drawing.Point(491, 395);
             this.btnAgregarArticuloDesdeLista.Name = "btnAgregarArticuloDesdeLista";
-            this.btnAgregarArticuloDesdeLista.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregarArticuloDesdeLista.Size = new System.Drawing.Size(141, 37);
             this.btnAgregarArticuloDesdeLista.TabIndex = 5;
-            this.btnAgregarArticuloDesdeLista.Text = "Agregar Articulo";
+            this.btnAgregarArticuloDesdeLista.Text = "Agregar";
             this.btnAgregarArticuloDesdeLista.UseVisualStyleBackColor = true;
             this.btnAgregarArticuloDesdeLista.Click += new System.EventHandler(this.btnAgregarArticuloDesdeLista_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(410, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nombre de artículo:";
+            // 
+            // txtFiltro
+            // 
+            this.txtFiltro.Location = new System.Drawing.Point(517, 51);
+            this.txtFiltro.Name = "txtFiltro";
+            this.txtFiltro.Size = new System.Drawing.Size(179, 20);
+            this.txtFiltro.TabIndex = 7;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(702, 49);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 8;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Categoría";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(139, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Marca";
             // 
             // FrmListarArticulo
             // 
@@ -114,6 +169,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtFiltro);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnAgregarArticuloDesdeLista);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lblListadoArticulos);
@@ -142,5 +202,10 @@
         private System.Windows.Forms.Label lblListadoArticulos;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnAgregarArticuloDesdeLista;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFiltro;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
